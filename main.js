03e0dc527f9721425bb3668315a56b708bb66df0
Joaquin Cabrera <joaquincccr@gmail.com>
2023-05-02 14:45:55 -0300

    Modificaciones para pre entrega 2

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 class Usuario {
-    constructor (nombre, apellido, dni){
+    constructor (nombre, apellido, dni, saldo){
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
+        this.saldo = saldo;
     }
 }
 
-const usuarioJuan = new Usuario("Juan", "Gomez", 1111);
-const usuarioPepe = new Usuario("Pepe", "Perez", 2222);
-const usuarioPablo = new Usuario("Pablo", "Gomez",3333);
+const usuarioJuan = new Usuario("Juan", "Gomez", 1111, 500);
+const usuarioPepe = new Usuario("Pepe", "Perez", 2222, 200);
+const usuarioPablo = new Usuario("Pablo", "Gomez",3333, 1000);
 
 const arrayUsuarios = [];
 
@@ -23,32 +24,84 @@ console.log(arrayUsuarios);
 
 alert ("Bienvenido al banco Coder & Co.");
 
+arrayUsuarios.forEach (Usuario => {
+    console.log (Usuario);
+})
 
-let dni = parseInt(prompt("Por favor, ingrese su DNI: "));
-let usuario = arrayUsuarios.find(Usuario => Usuario.dni === dni);
+//let dni = parseInt(prompt("Por favor, ingrese su DNI: "));
 
-if (dni == usuario){
+do{
+    dni = prompt("Por favor, ingrese su DNI: ")
+} while (dni != Usuario.dni)
+
+if (dni === Usuario.dni){
     alert ("Bienvenido " + Usuario.nombre + " " + Usuario.apellido);
-}else{
-    alert ("El DNI ingresado no corresponde a ningun usuario, vuelva a intentar");
 }
 
 /*--------------------- VER SALDO DE CUENTA Y OPERACION DE DEPOSITO --------------------------------*/
-alert ("Su saldo es de $0")
 
-let saldoInicial = 0;
-let montoIngresado = parseInt(prompt("¿Cuanto dinero desea ingresar a su cuenta?"));
-console.log(montoIngresado);
-
-if (montoIngresado < 0){
-    alert ("Disculpe, el monto ingresado es incorrecto. Por favor ingrese un monto mayor a cero pesos.");
+function menu() {
+    let opcion = parseInt(prompt("Por favor, seleccione la operacion a realizar: \n 1) Consultar saldo de cuenta \n 2) Depositar \n 3) Salir"));
+    return opcion;
 }
 
-function suma (saldoInicial,montoIngresado){
-    return saldoInicial + montoIngresado;
+function consultaSaldo() {
+    let documento = parseInt(prompt("Por favor, ingrese nuevamente su DNI: "));
+    let usuario = arrayUsuarios.find(Usuario => Usuario.saldo === saldo);
+    console.log(cliente);
 }
-let resultadoSuma = suma(saldoInicial,montoIngresado);
-console.log(resultadoSuma)
-alert("Ahora saldo de su cuenta es de $ " + resultadoSuma);
 
-alert ("Gracias por confiar en Coder & Co. Hasta la proxima")
+function deposito(){
+    let dni = parseInt(prompt("Por favor, ingrese nuevamente su DNI: "));
+    let usuario = arrayUsuarios.find(Usuario => Usuario.dni);
+    let indice = arrayClientes.indexOf(Usuario);
+    let saldo = arrayUsuarios.find(Usuario => Usuario.saldo);
+    let deposito = parseInt(prompt("¿Cuanto dinero desea ingresar a su cuenta?"));
+    function suma (deposito,saldo){
+        return deposito + saldo
+    }
+    let nuevoSaldo = new Cliente(saldo);
+    arrayUsuarios.splice(indice, 1, nuevoSaldo);
+    console.log(arrayUsuarios);
+}
+
+function salir() {
+    alert("Gracias por confiar en Coder & Co. Hasta la proxima.");
+}
+
+let opcion = menu();
+switch (opcion) {
+    case 1:
+        consultaSaldo();
+        break;
+    case 2:
+        deposito();
+        break;
+    case 3:
+        salir();
+        break;
+    default:
+        alert("Opción incorrecta");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
