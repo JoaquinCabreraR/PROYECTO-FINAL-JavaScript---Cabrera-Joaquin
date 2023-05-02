5d625c9c3f271fa85b3cd5eba9c337fb63291329
Joaquin Cabrera <joaquincccr@gmail.com>
2023-05-02 00:11:01 -0300

    Carga de pre entrega 2 JS

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-alert ("Bienvenido al banco Coder & Co.")
-
-const usuario = "coder";
-const password = 1234;
-
-let usuarioCliente = usuario;
-let passwordCliente = password;
-
-do{
-    usuarioCliente = prompt("Ingrese su usuario");
-} while(usuarioCliente != usuario);
-console.log(usuarioCliente);
-
-do{
-    passwordCliente = prompt("Ingrese su contraseña")
-} while (passwordCliente != password);
-console.log(passwordCliente);
-
-if ((usuarioCliente == usuario)&&(passwordCliente == password)){
-    alert("Hola " + usuario + ". Buenos dias");
+class Usuario {
+    constructor (nombre, apellido, dni){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
 }
 
+const usuarioJuan = new Usuario("Juan", "Gomez", 1111);
+const usuarioPepe = new Usuario("Pepe", "Perez", 2222);
+const usuarioPablo = new Usuario("Pablo", "Gomez",3333);
+
+const arrayUsuarios = [];
+
+arrayUsuarios.push (usuarioJuan);
+arrayUsuarios.push (usuarioPepe);
+arrayUsuarios.push (usuarioPablo);
+
+console.log(arrayUsuarios);
+
+/*------------------------- INICIO DE SESION --------------------------------------*/
+
+
+alert ("Bienvenido al banco Coder & Co.");
+
+
+let dni = parseInt(prompt("Por favor, ingrese su DNI: "));
+let usuario = arrayUsuarios.find(Usuario => Usuario.dni === dni);
+
+if (dni == usuario){
+    alert ("Bienvenido " + Usuario.nombre + " " + Usuario.apellido);
+}else{
+    alert ("El DNI ingresado no corresponde a ningun usuario, vuelva a intentar");
+}
+
+/*--------------------- VER SALDO DE CUENTA Y OPERACION DE DEPOSITO --------------------------------*/
 alert ("Su saldo es de $0")
 
 let saldoInicial = 0;
@@ -29,10 +43,6 @@ console.log(montoIngresado);
 if (montoIngresado < 0){
     alert ("Disculpe, el monto ingresado es incorrecto. Por favor ingrese un monto mayor a cero pesos.");
 }
-/*else if (montoIngresado >= 0); {
-    let montoIngresado = parseInt(prompt('¿Cuanto dinero desea ingresar a su cuenta?'));
-console.log(montoIngresado);
-}*/
 
 function suma (saldoInicial,montoIngresado){
     return saldoInicial + montoIngresado;
