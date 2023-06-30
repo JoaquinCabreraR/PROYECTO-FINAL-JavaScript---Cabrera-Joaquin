2702962258a32a4aa5449ecd852cebe2676ef109
Joaquin Cabrera <joaquincccr@gmail.com>
2023-06-30 09:52:13 -0300

    Arreglos para re entrega, eliminacion alert

--- paginaPrincipal.js
+++ paginaPrincipal.js
@@ -22,55 +22,6 @@ console.log(arrayUsuarios);
 localStorage.setItem('usuarios', JSON.stringify(arrayUsuarios));
 
 /*------------------------------- MENU ------------------------------------------*/
-/*
-window.addEventListener('load', function() {
-    function menu() {
-        let opcion = parseInt(
-            prompt("Por favor, seleccione la operacion a realizar:\n1) Consultar saldo de cuenta\n2) Depositar dinero en cuenta")
-        );
-        return opcion;
-    }
-    
-    function consultaSaldo() {
-        let documento = parseInt(prompt("Por favor, ingrese nuevamente su DNI: "));
-        let usuario = arrayUsuarios.find((usuario) => usuario.dni === documento);
-        console.log(usuario.saldo);
-        alert("Su saldo es de $ " + usuario.saldo)
-    }
-    
-    function deposito() {
-        let documento = parseInt(prompt("Por favor, ingrese nuevamente su DNI: "));
-        let usuario = arrayUsuarios.find((usuario) => usuario.dni === documento);
-        let indice = arrayUsuarios.indexOf(usuario);
-        let saldo = usuario.saldo;
-        let deposito = parseInt(prompt("¿Cuánto dinero desea ingresar a su cuenta?"));
-    
-        function suma(deposito, saldo) {
-            return deposito + saldo;
-        }
-    
-        let nuevoSaldo = suma(deposito, saldo);
-        usuario.saldo = nuevoSaldo;
-        arrayUsuarios.splice(indice, 1, usuario);
-        console.log(arrayUsuarios);
-        localStorage.setItem('usuarios', JSON.stringify(arrayUsuarios));
-        alert("Su nuevo saldo es de $ " + nuevoSaldo)
-    }
-    
-    let opcion = menu();
-    switch (opcion) {
-        case 1:
-            consultaSaldo();
-            break;
-        case 2:
-            deposito();
-            break;
-        default:
-            alert("Opción incorrecta");
-            break;
-    }
-});
-*/
 
 window.addEventListener('load', function () {
     menu();
@@ -186,9 +137,6 @@ function deposito() {
         }
     });
 }
-
-
-
 
 /*------------------------------- CERRAR SESION ------------------------------------------*/
 
